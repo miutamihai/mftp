941c00c4960ee82863f3fe05f729f0f63d551bb9
mihaimiuta <mihaimiuta@proton.me>
2024-08-30 15:12:15 +0300

    [master] Log to buffer

--- mftp/main.go
+++ mftp/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"slices"
 )
 
 func main() {
+	var buffer bytes.Buffer
+
+	logger := log.New(&buffer, "mftp: ", log.LstdFlags)
+
 	names := []string{"mihai", "samy"}
 
 	for _, name := range slices.Backward(names) {
-		fmt.Printf("Name: %s\n", name)
+		logger.Printf("Name: %s\n", name)
 	}
+
+	fmt.Println("Buffer")
+	fmt.Printf("%s\n", &buffer)
 }
